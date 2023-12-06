03826b412e400b24352826caf162b978c45244d0
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2023-12-06 12:51:21 +0500

    filtered the files by arguments

--- index.py
+++ index.py
@@ -1,9 +1,25 @@
 import sys
 import os
 import calendar
+
 # First argument -e -a -c
 # Second argument  year/month
 # Third argument pathToFile
+
+
+def filter_files(path, year, month):
+    files_in_folder = os.listdir(folder_path)
+    file_names = []
+    for file_name in files_in_folder:
+        if month and year:
+            if str(year) in file_name and str(month) in file_name:
+                full_file_path = os.path.join(folder_path, file_name)
+                file_names.append(full_file_path)
+        else:
+            if str(year) in file_name:
+                full_file_path = os.path.join(folder_path, file_name)
+                file_names.append(full_file_path)
+    return file_names
 
 
 def get_arguments():
@@ -14,7 +30,7 @@ def get_arguments():
     if len(args) >= 2:
         year = int(args[0])
         month = int(args[1])
-        if(month > 12 or month < 1):
+        if (month > 12 or month < 1):
             raise Exception("Invalid Month")
         month = calendar.month_abbr[month]
     else:
@@ -27,7 +43,7 @@ try:
     if (sys.argv[1] not in ["-e", "-a", "-c"]):
         raise Exception("Invalid argument")
     year, month, folder_path = get_arguments()
-    print(year, month, folder_path)
+    filtered_files = filter_files(folder_path, year, month)
 except IndexError:
     print("Please provide all arguments")
 except ValueError:
