d717e4d68b1c034dcf3897f2181f067b06f5e30c
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2023-12-06 16:05:41 +0500

    added max min humid temp feature

--- index.py
+++ index.py
@@ -1,10 +1,66 @@
 import sys
 import os
 import calendar
+from datetime import datetime
 
 # First argument -e -a -c
 # Second argument  year/month
 # Third argument pathToFile
+
+
+def max_min_humid_temp_of_year(files):
+    max_temp = float('-inf')
+    min_temp = float('inf')
+    max_humidity = float('-inf')
+
+    date_max_temp = ''
+    date_min_temp = ''
+    date_max_humidity = ''
+
+    for file in files:
+        max_temp, date_max_temp, min_temp, date_min_temp, max_humidity, date_max_humidity = get_max_min_humid_temp(
+            file, max_temp, min_temp, max_humidity, date_max_temp, date_min_temp, date_max_humidity)
+
+    # Format and print the overall results
+    print(
+        f"Highest overall: {max_temp}C on {get_month(date_max_temp.month)} {date_max_temp.day}")
+    print(
+        f"Lowest overall: {min_temp}C on {get_month(date_min_temp.month)} {date_min_temp.day}")
+    print(
+        f"Humidity overall: {max_humidity}% on {get_month(date_max_humidity.month)} {date_max_humidity.day}")
+
+
+def get_month(month):
+    return datetime.strptime(str(month), '%m').strftime('%B')
+
+
+def get_max_min_humid_temp(path, max_temp, min_temp, max_humidity, date_max_temp, date_min_temp, date_max_humidity):
+    with open(path, 'r') as file:
+        next(file)  # Skip the header line
+        for line in file:
+            data = line.strip().split(',')
+            if len(data) > 1:
+                date = datetime.strptime(data[0], '%Y-%m-%d')
+                max_temp_val = float(data[1])
+                min_temp_val = float(data[3])
+                humidity_val = float(data[7])
+
+                if max_temp_val > max_temp:
+                    max_temp = max_temp_val
+                    date_max_temp = date
+
+                if min_temp_val < min_temp:
+                    min_temp = min_temp_val
+                    date_min_temp = date
+
+                if humidity_val > max_humidity:
+                    max_humidity = humidity_val
+                    date_max_humidity = date
+    return (
+        max_temp, date_max_temp,
+        min_temp, date_min_temp,
+        max_humidity, date_max_humidity
+    )
 
 
 def filter_files(path, year, month):
@@ -44,6 +100,9 @@ try:
         raise Exception("Invalid argument")
     year, month, folder_path = get_arguments()
     filtered_files = filter_files(folder_path, year, month)
+    if (sys.argv[1] == "-e"):
+        max_min_humid_temp_of_year(filtered_files)
+
 except IndexError:
     print("Please provide all arguments")
 except ValueError:
