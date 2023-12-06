9f29b34c87756de7093e2ba8dfece3643e49fa12
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2023-12-06 12:39:16 +0500

    converted argument values to integers

--- index.py
+++ index.py
@@ -1,19 +1,22 @@
 import sys
 import os
-
+import calendar
 # First argument -e -a -c
 # Second argument  year/month
 # Third argument pathToFile
 
 
-def getArguments():
-    path = sys.argv[3]
-    if not os.path.exists("." + path):
+def get_arguments():
+    path = "." + sys.argv[3]
+    if not os.path.exists(path):
         raise Exception("Path Does not exist")
     args = sys.argv[2].split("/")
     if len(args) >= 2:
-        year = args[0]
-        month = args[1]
+        year = int(args[0])
+        month = int(args[1])
+        if(month > 12 or month < 1):
+            raise Exception("Invalid Month")
+        month = calendar.month_abbr[month]
     else:
         year = args[0]
         month = None
@@ -23,9 +26,11 @@ def getArguments():
 try:
     if (sys.argv[1] not in ["-e", "-a", "-c"]):
         raise Exception("Invalid argument")
-    year, month, path = getArguments()
-    getArguments()
+    year, month, folder_path = get_arguments()
+    print(year, month, folder_path)
 except IndexError:
     print("Please provide all arguments")
+except ValueError:
+    print("Please pass correct values")
 except Exception as e:
     print(e)
